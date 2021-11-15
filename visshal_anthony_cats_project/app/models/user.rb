0c94a5c2827bb19fde1c04cd85f7ae4467436af1
Visshal Suresh <skvisshal@gmail.com>
2021-11-15 14:31:36 -0500

    user model and migration

---
+++ visshal_anthony_cats_project/app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+    validates :user_name, :password_digest, :session_token, presence: true
+    validates :user_name, :session_token, uniqueness: true
+
+    after_initialize :endure_session_token
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+
+        return nil if user.nil?
+
+        user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+    
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64(16)
+        self.save!
+        self.session_token
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64(16)
+    end
+end
---
+++ visshal_anthony_cats_project/db/migrate/20211115190620_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :user_name, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+    add_index :users, :user_name, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
